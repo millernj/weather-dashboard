ebc50f52a9c5aa6eae369e79dad8b5f2b6e76dd7
millernj <noahjmiller17@gmail.com>
2020-02-06 13:29:54 -0800

    adding current location handling on page load, local storage for past queries, and getting weather information

--- script.js
+++ script.js
@@ -1,4 +1,97 @@
 $('#search-submit').click((event) => {
   event.preventDefault();
   const query = $('#search-city').val();
+  getWeatherWithUV({q: query}).then((response) => {
+    renderCurrentWeather(response);
+  })
+  let queries = localStorage.getItem('cities');
+  if (queries) {
+    queries = JSON.parse(queries);
+    queries.push(query);
+    localStorage.setItem('cities', JSON.stringify(queries));
+  } else {
+    localStorage.setItem('cities', JSON.stringify([query]));
+  }
+
+})
+
+const renderCurrentWeather = (weatherData) => {
+
+  const panelElement = $('#weather-panel');
+
+  const cityElement = $('#current-city');
+  const dateElement = $('#current-date');
+  const iconElement = $('#current-weather-icon');
+
+  const tempElement = $('#current-temp');
+  const humidityElement = $('#current-humidity');
+  const windspeedElement = $('#current-windspeed');
+  const uvElement = $('#current-uv');
+
+  const { 
+    name, 
+    main: {
+      temp, 
+      humidity
+    },
+    wind: {
+      speed
+    },
+    weather,
+    uvi
+  } = weatherData;
+
+  cityElement.text(name);
+  dateElement.text(moment().format('(MM/DD/YYYY)'));
+
+  iconElement.attr('src', getWeatherIconUrl(weather[0].icon));
+
+  tempElement.children('span').text(convertToFahrenheit(temp).toFixed(2));
+  humidityElement.children('span').text(humidity);
+  windspeedElement.children('span').text(speed);
+  uvElement.children('span').text(uvi);
+
+
+  if (panelElement.hasClass('hidden')) {
+    panelElement.removeClass('hidden');
+  }
+}
+
+const getWeatherIconUrl = (iconCode) => {
+  return `http://openweathermap.org/img/wn/${iconCode}.png`;
+}
+
+const getLocation = () => {
+  const apiKey = 'fb15c41db5fee25ded055f95bf360b72';
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: 'https://jsonip.com/',
+      method: 'GET'
+    })
+      .then(response => {
+        const { ip } = response;
+        return $.ajax({
+          url: `http://api.ipstack.com/${ip}?access_key=${apiKey}`
+        })
+      })
+      .then(response => {
+        console.log(response);
+        resolve(response);
+      })
+      .catch(error => {
+        console.error(error);
+        reject(error);
+      })
+  })
+}
+
+
+
+$(document).ready(() => {
+  getLocation().then(response => {
+    const { city } = response;
+    getWeatherWithUV({q: city}).then((response) => {
+      renderCurrentWeather(response);
+    })
+  })
 })
---
+++ weather.js
@@ -0,0 +1,69 @@
+const apiKey = '531c059ea600bda3cbe5aee75ec46e5f';
+
+const getWeather = (params) => {
+
+  let uri = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}`;
+  for (const param in params) {
+    if (params.hasOwnProperty(param)) {
+      const value = params[param];
+      uri+=`&${param}=${value}`;
+    }
+  }
+  const settings = {
+    url: uri,
+    method: 'GET'
+  }
+
+  return $.ajax(settings);
+}
+
+const getUVIndex = ({lat, lon}) => {
+
+  const uri = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+  const settings = {
+    url: uri,
+    method: 'GET'
+  }
+
+  return $.ajax(settings);
+}
+
+const getWeatherWithUV = (params) => {
+  return new Promise((resolve, reject) => {
+    let result = {};
+    getWeather(params)
+      .then((response) => {
+        const { coord } = response;
+        result = response;
+        return getUVIndex(coord);
+      })
+      .then((response) => {
+        const { value: uvi } = response;
+        result.uvi = uvi;
+        resolve(result);
+      })
+      .catch((error) => {
+        console.error(error);
+        reject(error);
+      })
+  })
+}
+
+const convertToCelcius = (temperature, inputUnit='K') => {
+  switch (inputUnit) {
+    case 'K':
+      return parseFloat(temperature) - 273.15;
+    case 'F':
+      return (parseFloat(temperature) - 32) * 5 / 9;
+  }
+}
+
+const convertToFahrenheit = (temperature, inputUnit='K') => {
+  switch (inputUnit) {
+    case 'K':
+      return (convertToCelcius(temperature) * 9 / 5) + 32;
+    case 'C':
+      return (parseFloat(temperature) * 9 / 5) + 32;
+  }
+}
+
